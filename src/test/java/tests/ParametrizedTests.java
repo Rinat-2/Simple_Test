d254d333044ee6174988ec168c19e9df8569183e
Rinat <rinat.astr2@gmail.com>
2020-08-23 21:18:11 +0600

    commit

---
+++ src/test/java/tests/ParametrizedTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ParametrizedTests {
+    static Logger logger = LoggerFactory.getLogger(ParametrizedTests.class);
+
+
+    @Test
+    @Tag("apple_tag")
+    void paramitrizedStringTest() {
+        String a = System.getProperty("test"); // from terminal, gradle apple_tests -Dtest=param
+
+        logger.info("\na = " + a);
+        assertEquals("paramtest", a, "b = " + a);
+    }
+}
